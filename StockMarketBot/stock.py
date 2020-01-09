38f032fc660e95317ac8bdb850ed51cca271a560
gary210741 <chou105014@gmail.com>
2020-01-09 23:36:59 +0800

    final push...I hope

--- StockMarketBot/stock.py
+++ StockMarketBot/stock.py
@@ -3,6 +3,7 @@ from imgurpython import ImgurClient
 import twstock, time, matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
+plt.rcParams["font.family"]="DFKai-SB"
 
 matplotlib.use("Agg")
 
