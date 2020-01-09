75de9a9933790f3465a608420697b973acdebeba
gary210741 <chou105014@gmail.com>
2020-01-09 22:09:51 +0800

    debugging of real time price

--- StockMarketBot/app.py
+++ StockMarketBot/app.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
 	InvalidSignatureError
 )
 from linebot.models import *
-import twstock, random, time, matplotlib, os, bot
+import twstock, random, time, matplotlib, os, stock
 import matplotlib.pyplot as plt
 import pandas as pd
 matplotlib.use('Agg')
@@ -18,9 +18,9 @@ matplotlib.use('Agg')
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('2xKVmBADbCqgHkTah4XjfrZ8GL01xFN1pV6BxB9tlbEkyeGyPWbPv7IE8iJlSbgc/9UDkNqEXoAYY193w1ED6I2EEJcdgFBQJcrnujJYEDj3UmQn1CpwgknsY7xSc3LYDaW2G9MDDAP44ch1E7fCGwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi("2xKVmBADbCqgHkTah4XjfrZ8GL01xFN1pV6BxB9tlbEkyeGyPWbPv7IE8iJlSbgc/9UDkNqEXoAYY193w1ED6I2EEJcdgFBQJcrnujJYEDj3UmQn1CpwgknsY7xSc3LYDaW2G9MDDAP44ch1E7fCGwdB04t89/1O/w1cDnyilFU=")
 # Channel Secret
-handler = WebhookHandler('696fbdfbd93968f7f833125cc0b6f456')
+handler = WebhookHandler("696fbdfbd93968f7f833125cc0b6f456")
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -40,6 +40,10 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+	'''
+	#
+	# 這部分的目的是取得user data
+	#
 	if event.message.text.lower()=='me':
 		userId = str(event.source.user_id)
 		message = TextSendMessage(userId)
@@ -53,8 +57,15 @@ def handle_message(event):
 					TextSendMessage('picture url: ' + profile.picture_url),
 					TextSendMessage('status_message: ' + status),
 					]
+	'''
+	if event.message.text.startswith("#"):
+		stockNO = event.message.text[1:]
+		rTinfo = stock.stockRT(stockNO) #Real Time Info
+		message = TextSendMessage(rTinfo)
+	elif event.message.text.startswith("/"):
+		pass
 	elif (event.message.text.lower() == "help"):
-		help_log = "完整的查詢天氣，請輸入[縣市名][天氣] e.g. '新北市天氣如何?'"
+		help_log = ""
 		message = TextSendMessage(help_log)
 	else:
 		invalidSentence = "無法辨識的內容，輸入[help]獲得更多資訊"
